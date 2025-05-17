c181b0c57df218652ad315cece17dccc8ef5999a
ashishhcodes <khandekaraa123@gmail.com>
2025-05-17 16:07:03 +0530

    basics practice

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 1223334444
+let accountEmail = "abk@gmail.com"
+var accountPassword = "2345"
+accountCity = "Pune"
+console.log(accountPassword);
+// accountEmail = 234
+accountPassword = "xyz"
+console.log(accountPassword);
+
+console.log(accountCity);
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
