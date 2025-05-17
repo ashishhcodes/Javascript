308dedf84d7ceb2a7b10387906919cbacb6beedf
ashishhcodes <khandekaraa123@gmail.com>
2025-05-17 16:49:08 +0530

    learned datatype conversion

---
+++ basics/datatype_conversion.js
@@ -0,0 +1,39 @@
+// let score = "35"
+
+// console.log(typeof score);  //op - string
+
+// console.log(typeof (score)); //op - string
+
+// let valueInNumber = Number(score) 
+
+ 
+// console.log(typeof valueInNumber); //op - number
+// console.log(valueInNumber); //op - 35
+
+
+// // some more 
+
+// let shape = "square"
+
+// console.log(typeof shape); //op - string
+
+// let shapeInNum = Number(shape)
+
+// console.log(typeof shapeInNum); //op - number
+
+// console.log(shapeInNum); //op - NaN
+
+
+// some more
+
+let random = "javascript"
+
+console.log(typeof random); //op - string
+
+console.log(random); //op - javascript
+
+
+let newName = Number(random) 
+
+console.log(typeof newName); //op - number
+console.log( newName); //op - NaN
