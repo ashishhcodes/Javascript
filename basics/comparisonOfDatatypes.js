d6391cfd3cd9329c56e2dc9f1b0cfa093e41a48a
ashishhcodes <khandekaraa123@gmail.com>
2025-05-18 16:42:58 +0530

    datatype Comparison

---
+++ basics/comparisonOfDatatypes.js
@@ -0,0 +1,22 @@
+// normal comparison
+console.log(2 < 2); //false
+console.log(2 > 1); //true
+console.log(2 <= 3);//true
+console.log(2 == 2);//true
+// console.log(2 >>> 2);
+console.log(2 <= 2);//true
+// console.log(2 << 2);
+
+console.log("2" > 1);//true
+console.log("02" > 1);//true
+console.log(2 > "4");//false
+
+
+console.log(null < 0);//false
+console.log(null > 0);//false
+console.log(null <= 0);//true
+console.log(null >= 0);//true
+console.log(null == 0);//false
+
+
+
