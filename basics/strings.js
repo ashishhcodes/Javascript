92ea208bf4fe8d517cd45147e42542dfb23c3e33
ashishhcodes <khandekaraa123@gmail.com>
2025-05-20 16:02:25 +0530

    stringPrototypeExplore

--- basics/strings.js
+++ basics/strings.js
@@ -28,6 +28,23 @@ console.log(gameName.substring(0,5)); //socce
 // console.log(gameName.trim(2));
 console.log(gameName.replace('s','t')); //op: toccer
 
+// some protype methods with meaning 
+
+console.log(gameName.charCodeAt(2)); //op: 99; Returns the Unicode (ASCII code) of the character at the given index.
+
+console.log("c".charCodeAt()); // 99
+
+
+console.log("Hi".concat(" there")); // op: Hi there; Used to join two or more strings together.
+//  It works the same as +, but as a method.
+
+console.log("Javascript".includes("asc")); //op: true; Checks whether a string contains a specific substring. Returns true or false.
+
+console.log("banana".indexOf("a")); //op: 1; Returns the first position where the substring appears. If not found, returns -1.
+
+
+
+
 
 
 
