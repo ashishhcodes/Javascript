444b58975103e57c20b5b02e77bf38e985e714f2
ashishhcodes <khandekaraa123@gmail.com>
2025-06-03 18:17:08 +0530

    Scope

--- basics_03/02_scopes.js
+++ basics_03/02_scopes.js
@@ -1,9 +1,66 @@
 // SCOPE {}
 
-let a = 10
-const b = 20
-var c = 30
+let a = 200
+
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log(b);
+    
+}
 
 console.log(a);
-console.log(b);
+// console.log(b);
 console.log(c);
+
+
+// Nested scope
+
+function one(){
+    const username = "Rish"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website); we cannot access it outside
+    
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "VM"
+    if (username === "VM") {
+        const website = " youtue"
+        console.log(username + website);
+    }
+    // console.log(website); err: cannot access here
+}
+// console.log(username); err: cannot access here
+
+
+// +++++++++++++++++interesting que +++++++++++++++
+
+console.log(addOne(5)); //op: 6
+
+function addOne(num){
+
+    return num + 1
+}
+// addOne(5)
+
+
+
+// addTwo(5) //err: Cannot access 'addTwo' before initialization
+const addTwo = function (num) {
+    return num + 2
+}
+
+addTwo(5)
+
+// in first case we have just declared the function so it can be accessable before initialization also
+// but in second case we've declared aswell as hold the value n=inside a variable so thats why we cannot access it before initialization
+// hoisting concept  
