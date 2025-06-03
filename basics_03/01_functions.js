e326e28265d49b55e689c40ed3abe4cff6f3f3f3
ashishhcodes <khandekaraa123@gmail.com>
2025-06-03 16:47:35 +0530

    functionsIntro1

--- basics_03/01_functions.js
+++ basics_03/01_functions.js
@@ -1,67 +1,67 @@
 
 
-// function sayMyName(){
+function sayMyName(){
    
-//     console.log("R");
-//     console.log("I");
-//     console.log("S");
-//     console.log("H");
-//     console.log("I");
+    console.log("R");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+    console.log("I");
 
-// }
-// sayMyName() 
-// //op:
-// // R
-// // I
-// // S
-// // H
-// // I
+}
+sayMyName() 
+//op:
+// R
+// I
+// S
+// H
+// I
 
-// // sayMyName if we write only like this, then its the reference of that function
-// // sayMyName() if we write like this with parenthesis, then its the execution of that function
+// sayMyName if we write only like this, then its the reference of that function
+// sayMyName() if we write like this with parenthesis, then its the execution of that function
 
-// // console.log(typeof sayMyName);
-
-
-// // function to add two numbers: 
-
-// function addTwoNums(num1,num2){
-//     console.log(num1 + num2);
-// }
-// addTwoNums(3,2) //op: 5
-// addTwoNums(3,"2") //op: 32 
-// addTwoNums("3","2") //op: 32
-// addTwoNums("3","2") //op: 32
-// addTwoNums("a",2) //op: a2
-// addTwoNums(3,"b") //op: 3b
-// addTwoNums("3",null) //op: 3null
-// addTwoNums(null,null) //op:0
-// addTwoNums(null,2) //op: 2
-
-// function subtractTwoNums(n1,n2){
-
-//     let result = n1 - n2
-//     return result
-// }
-// result = subtractTwoNums(10,5)
-
-// console.log("Result : ", result); //op: esult : 5
+// console.log(typeof sayMyName);
 
 
-// console.log(subtractTwoNums(10,5));
+// function to add two numbers: 
 
-// function loginUserMSG(username){
-//     if (username === undefined) {
-//         // or same we can write like: both are same
-//         // if (!username) { 
-//         console.log("Please enter a username");
+function addTwoNums(num1,num2){
+    console.log(num1 + num2);
+}
+addTwoNums(3,2) //op: 5
+addTwoNums(3,"2") //op: 32 
+addTwoNums("3","2") //op: 32
+addTwoNums("3","2") //op: 32
+addTwoNums("a",2) //op: a2
+addTwoNums(3,"b") //op: 3b
+addTwoNums("3",null) //op: 3null
+addTwoNums(null,null) //op:0
+addTwoNums(null,2) //op: 2
+
+function subtractTwoNums(n1,n2){
+
+    let result = n1 - n2
+    return result
+}
+result = subtractTwoNums(10,5)
+
+console.log("Result : ", result); //op: esult : 5
+
+
+console.log(subtractTwoNums(10,5));
+
+function loginUserMSG(username){
+    if (username === undefined) {
+        // or same we can write like: both are same
+        // if (!username) { 
+        console.log("Please enter a username");
         
-//     }
-//     return `${username} just logged in`
-// }
-// console.log(loginUserMSG("AkhilJ")); //op: AkhilJ just logged in
-// // console.log(loginUserMSG("")); //op:  just logged in
-// console.log(loginUserMSG()); //op: undefined just logged in
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMSG("AkhilJ")); //op: AkhilJ just logged in
+// console.log(loginUserMSG("")); //op:  just logged in
+console.log(loginUserMSG()); //op: undefined just logged in
 
 function calculateCartPrice(num1){
 
@@ -92,7 +92,6 @@ function newObjFun(anyobject) {
 }
 
 newObjFun(user) //op: username is Jamval and id is 1
-// console.log(newObjFun(user));
 
 // another way to pass object
 
