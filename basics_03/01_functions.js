41a28000ec9300b40ea0d1eaa12295f84645ad27
ashishhcodes <khandekaraa123@gmail.com>
2025-06-03 16:43:22 +0530

    functionsIntro

--- basics_03/01_functions.js
+++ basics_03/01_functions.js
@@ -38,16 +38,78 @@
 // addTwoNums(null,null) //op:0
 // addTwoNums(null,2) //op: 2
 
-function subtractTwoNums(n1,n2){
+// function subtractTwoNums(n1,n2){
 
-    let result = n1 - n2
-    return result
-}
-result = subtractTwoNums(10,5)
+//     let result = n1 - n2
+//     return result
+// }
+// result = subtractTwoNums(10,5)
 
-console.log("Result : ", result); //op: Result : 5
+// console.log("Result : ", result); //op: esult : 5
 
 
 // console.log(subtractTwoNums(10,5));
 
+// function loginUserMSG(username){
+//     if (username === undefined) {
+//         // or same we can write like: both are same
+//         // if (!username) { 
+//         console.log("Please enter a username");
+        
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(loginUserMSG("AkhilJ")); //op: AkhilJ just logged in
+// // console.log(loginUserMSG("")); //op:  just logged in
+// console.log(loginUserMSG()); //op: undefined just logged in
+
+function calculateCartPrice(num1){
+
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,100)); //2
+
+
+// ... rest operator also spread  operator does looks same but its for array and object merging
+function calculateCart(...num2){ //...rest operator to accept more inputs
+    return num2
+}
+
+console.log(calculateCart(100,300,500,734,394)); //op: [ 100, 300, 500, 734, 394 ]
+
+
+// passing object as parameter in function
+
+const user = {
+    username: "Jamval",
+    id: 1
+}
+
+function newObjFun(anyobject) {
+    console.log(`username is ${anyobject.username} and id is ${anyobject.id}`);
+    
+}
+
+newObjFun(user) //op: username is Jamval and id is 1
+// console.log(newObjFun(user));
+
+// another way to pass object
+
+newObjFun({
+    username: "Akhil",
+    id: 1
+}) //op: username is Akhil and id is 1      
+
+const myNewArray = [200,400,100,500]
+
+function SecondValueReturn(getArray){
+    return getArray[1]
+}
+
+console.log(SecondValueReturn(myNewArray)); //400
+
+// anothr way to pass full another array
+
+console.log(SecondValueReturn([200,1,3,4,299])); //op: 1
 
