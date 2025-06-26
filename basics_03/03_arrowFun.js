befb592f514c2653976fbb0c8870bb7774fc768b
ashishhcodes <khandekaraa123@gmail.com>
2025-06-26 15:43:38 +0530

    Arrow

--- basics_03/03_arrowFun.js
+++ basics_03/03_arrowFun.js
@@ -44,9 +44,31 @@ const addTwo = (num1, num2) => {
 }
 console.log(addTwo(3,2));
 
-
-// Implicit return arrow function (one line)
+// explicitly retutn is when we write return and
+// Implicit return which is arrow function (one line)
 
 const addFour = (n1,n2,n3,n4) => n1+n2+n3+n4
 
 console.log(addFour(2,4,6,8));
+
+
+// TIP/NOTE : when we use curly braces{} then we have to write return 
+// when we use paranthetis () then we don't need to write written 
+// const addFour = (n1,n2,n3,n4) => n1+n2+n3+n4 we can write like this also and 
+// const addFour = (n1,n2,n3,n4) => (n1+n2+n3+n4) like this also 
+// this concept will help in react
+// 
+
+// and when we want to return object then also we have to write paranthetis around object's curly braces 
+// otherwise it will give you undefined
+
+// e.g
+
+const addnew = (n1,n2) => {username: "YA"}
+
+console.log(addnew(2,3)); //undefined
+
+// but when we do 
+const addAgain = (n,m) => ({username: "AJ"})
+
+console.log(addAgain(2,3)); //{ username: 'AJ' }
