748af3f0c1ba9ddb1c71c2b8eae9e027d6b9fa23
ashishhcodes <khandekaraa123@gmail.com>
2025-06-29 19:51:33 +0530

    iterationals

---
+++ iterations_05/one.js
@@ -0,0 +1,23 @@
+// // for loop
+
+// // const array = [1,2,3,4,5];
+// for (let i = 0; i < 10; i++) {
+//     // const element = i;
+//     if (i == 5) {
+//         console.log("5 is best number");
+        
+//     }
+//     console.log(i);
+    
+// }
+// // console.log(i);
+
+for (let i = 1; i <= 10; i++) {
+        console.log(`Table of ${i}`);
+
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i*j}`);        
+    }    
+    console.log();
+    
+}
