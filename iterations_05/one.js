85596e112efb6ebd2946a3d4e30dbb766f168da8
ashishhcodes <khandekaraa123@gmail.com>
2025-06-30 16:44:11 +0530

    forLoop

--- iterations_05/one.js
+++ iterations_05/one.js
@@ -12,12 +12,42 @@
 // }
 // // console.log(i);
 
-for (let i = 1; i <= 10; i++) {
-        console.log(`Table of ${i}`);
+// for (let i = 1; i <= 10; i++) {
+//         console.log(`Table of ${i}`);
 
-    for (let j = 1; j <= 10; j++) {
-        console.log(`${i} * ${j} = ${i*j}`);        
-    }    
-    console.log();
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);        
+//     }    
+//     console.log();
     
+// }
+
+// loop with array
+
+// let  myArray = ["mustang", "lamborgini", "ferrari", "porsche"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+// break and continue
+
+// for (let index = 1; index <=20; index++) {
+//     if (index == 5) {
+//         console.log(`detected 5`);
+        
+//         break
+//     }
+//     console.log(`value of index is ${index}`);    
+// }
+
+for (let index = 1; index <=20; index++) {
+if (index == 5) {
+    console.log(`detected 5`);
+    continue
+}
+console.log(`value of index is ${index}`);    
 }
